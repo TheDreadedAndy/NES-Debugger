3c92e5edf77eeb6e91dd30078483f47bc0f19e1f
Andrew Spaulding <andyespaulding@gmail.com>
2019-04-22 09:30:02 -0400

    Cleaned up makefile, commented SDL tester

--- src/sdl.c
+++ src/sdl.c
@@ -6,23 +6,29 @@
  */
 
 int main(int argc, char* argv[]) {
+  // Suppress GCC complaints.
+  // These variables need to be here for SDL to work.
   (void)argc;
   (void)argv;
 
-  SDL_Window *window;
+  // Init SDL's video system.
   SDL_Init(SDL_INIT_VIDEO);
 
+  // Create window.
+  SDL_Window *window;
   window = SDL_CreateWindow("NES Debugger", SDL_WINDOWPOS_CENTERED,
            SDL_WINDOWPOS_CENTERED, 256, 240, SDL_WINDOW_RESIZABLE);
 
+  // Check for failure.
   if (window == NULL) {
     printf("SDL failed\n");
   }
 
+  // Hold the window for 3 seconds.
   SDL_Delay(3000);
 
+  // Close the window and exit.
   SDL_DestroyWindow(window);
-
   SDL_Quit();
   return 0;
 }

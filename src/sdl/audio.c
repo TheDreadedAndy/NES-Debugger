4b4319cb761366218904fba7420643f8e8742013
TheDreadedAndy <andyespaulding@gmail.com>
2019-08-17 17:17:41 -0400

    audio ring buffer

--- src/sdl/audio.c
+++ src/sdl/audio.c
@@ -4,20 +4,30 @@
 
 #include <stdlib.h>
 #include <stdbool.h>
-#include <string.h>
 #include <SDL2/SDL.h>
 #include "./audio.h"
 #include "../util/util.h"
 
 // The max number of samples the buffer can hold.
 // Must be a power of 2.
-#define BUFFER_SIZE 512
+#define DEVICE_BUFFER_SIZE 1024U
+#define EMU_BUFFER_SIZE (DEVICE_BUFFER_SIZE << 1U)
+
+/*
+ * The emulation audio buffer is implemented as a ring buffer to
+ * increase speed.
+ */
+typedef struct audio_buffer {
+  float *buffer;
+  size_t base;
+  size_t size;
+} buffer_t;
 
 /*
  * Audio samples are stored in a buffer, which is queued when
  * audio_play_frame() is called.
  */
-static float *audio_buffer = NULL;
+static buffer_t *audio_buffer = NULL;
 static size_t buffer_slot = 0;
 
 /*
@@ -25,6 +35,12 @@ static size_t buffer_slot = 0;
  * SDL_OpenAudioDevice().
  */
 static SDL_AudioDeviceID audio_device = 0;
+
+/* Helper functions */
+void audio_callback(void *userdata, uint8_t *stream, int len);
+float audio_buffer_read(size_t index);
+void audio_buffer_write(size_t index, float val);
+void audio_buffer_set_base(size_t offset);
 
 /*
  * Initializes the audio system by allocating the buffer and opening the device.
@@ -37,19 +53,20 @@ bool audio_init(void) {
   want.freq = 44100;
   want.format = AUDIO_F32SYS;
   want.channels = 1;
-  want.samples = BUFFER_SIZE;
-  // Samples are added by audio_play_frame().
-  want.callback = NULL;
+  want.samples = DEVICE_BUFFER_SIZE;
+  want.callback = &audio_callback;
 
-  // The device is initialized.
+  // Init the device.
   audio_device = SDL_OpenAudioDevice(NULL, 0, &want, &have, 0);
   if (audio_device == 0) {
     fprintf(stderr, "Failed to open audio device\n");
     return false;
   }
 
-  // The audio buffer is allocated.
-  audio_buffer = xcalloc(sizeof(float), BUFFER_SIZE);
+  // Prepare the audio buffer.
+  audio_buffer = xcalloc(1, sizeof(buffer_t));
+  audio_buffer->buffer = xcalloc(EMU_BUFFER_SIZE, sizeof(float));
+  audio_buffer->size = EMU_BUFFER_SIZE;
 
   // Unpause the device and return success.
   SDL_PauseAudioDevice(audio_device, 0);
@@ -59,37 +76,49 @@ bool audio_init(void) {
 /*
  * Adds a sample to the audio buffer.
  *
- * Does nothing if audio has not been initialized.
+ * Does nothing if the buffer is full or audio has not been initialized.
  */
 void audio_add_sample(float sample) {
   // If audio is not ready, we do nothing.
-  if (audio_device == 0) { return; }
+  if ((audio_device == 0) || (buffer_slot >= EMU_BUFFER_SIZE)) { return; }
 
   // Otherwise, we add the sample to the buffer.
-  audio_buffer[buffer_slot] = sample;
+  // The buffer must be protected from audio callbacks while this is done.
+  SDL_LockAudioDevice(audio_device);
+  audio_buffer_write(buffer_slot, sample);
   buffer_slot++;
+  SDL_UnlockAudioDevice(audio_device);
 
-  // If the buffer has filled, we queue it.
-  if (buffer_slot >= BUFFER_SIZE) { audio_play_frame(); }
   return;
 }
 
 /*
- * Empties the audio buffer to the device, then resets the buffer.
- *
- * Does nothing if audio has not been initialized.
+ * Empties the audio buffer to the device, then resets the emulation buffer.
+ * If the emulation audio buffer cannot fill the device audio buffer,
+ * the device buffer is topped off with the last sample of the emulation
+ * buffer.
  */
-void audio_play_frame(void) {
-  // If audio has not been initialized, we return.
-  if (audio_device == 0) { return; }
+void audio_callback(void *userdata, uint8_t *stream, int len) {
+  // Get the device buffer and its size from the inputs.
+  size_t device_buffer_size = len / sizeof(float);
+  float *device_buffer = (float*) stream;
+  (void)userdata;
 
-  // Otherwise, queue the buffered samples and clear the buffer.
-  if (SDL_QueueAudio(audio_device, (void*) audio_buffer,
-      sizeof(float) * buffer_slot) < 0) {
-    fprintf(stderr, "Failed to play audio: %s\n", SDL_GetError());
+  // Fill in the device buffer using the emulation audio buffer.
+  for (size_t i = 0; i < device_buffer_size; i++) {
+    float sample = (i < buffer_slot) ? audio_buffer_read(i)
+                                     : audio_buffer_read(buffer_slot - 1);
+    device_buffer[i] = sample;
   }
-  buffer_slot = 0;
-  memset(audio_buffer, 0, sizeof(float) * BUFFER_SIZE);
+
+  // Update the emulation audio buffer.
+  if (device_buffer_size < buffer_slot) {
+    buffer_slot = buffer_slot - device_buffer_size;
+    audio_buffer_set_base(device_buffer_size);
+  } else {
+    buffer_slot = 0;
+  }
+
   return;
 }
 
@@ -106,5 +135,35 @@ void audio_close(void) {
   SDL_CloseAudioDevice(audio_device);
   free(audio_buffer);
 
+  return;
+}
+
+/*
+ * Reads a sample from the audio buffer.
+ *
+ * Assumes the audio buffer is non-null.
+ */
+float audio_buffer_read(size_t index) {
+  return audio_buffer->buffer[(audio_buffer->base + index)
+                                    % audio_buffer->size];
+}
+
+/*
+ * Writes a sample to the audio buffer.
+ *
+ * Assumes the audio buffer is non-null.
+ */
+void audio_buffer_write(size_t index, float val) {
+  audio_buffer->buffer[(audio_buffer->base + index) % audio_buffer->size] = val;
+  return;
+}
+
+/*
+ * Adds the given offset to the audio buffer base.
+ *
+ * Assumes the audio buffer has been initialized.
+ */
+void audio_buffer_set_base(size_t offset) {
+  audio_buffer->base = (audio_buffer->base + offset) % audio_buffer->size;
   return;
 }
--- src/sdl/audio.h
+++ src/sdl/audio.h
@@ -10,9 +10,6 @@ bool audio_init(void);
 // Adds a sample to the sample buffer.
 void audio_add_sample(float sample);
 
-// Plays the sample buffer and empties it.
-void audio_play_frame(void);
-
 // Closes the audio device and frees the buffer.
 void audio_close(void);
 
